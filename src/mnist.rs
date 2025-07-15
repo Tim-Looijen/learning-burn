9efabed52faba65afbe9ef128b10a45c4b083540
Tim Looijen <tim@lotan.nl>
2025-07-15 11:28:39 +0200

    Experimenting with Burn tensors

--- src/mnist.rs
+++ src/mnist.rs
@@ -1,6 +1,7 @@
+use burn::tensor::{Tensor, TensorData};
 use byteorder::{BigEndian, ReadBytesExt};
-use ndarray::prelude::*;
 use ndarray::{Array, Array2};
+use ndarray::{Data, prelude::*};
 use std::fmt::Debug;
 use std::fs::File;
 use std::io::{BufReader, Read};
@@ -9,7 +10,6 @@ const TEST_IMAGES_FILE: &str = "data/t10k-images.idx3-ubyte";
 const TEST_LABELS_FILE: &str = "data/t10k-labels.idx1-ubyte";
 const TRAIN_IMAGES_FILE: &str = "data/train-images.idx3-ubyte";
 const TRAIN_LABELS_FILE: &str = "data/train-labels.idx1-ubyte";
-const IMAGE_SIZE: usize = 784;
 
 fn read_mninst_file(path: &str) -> std::io::Result<(usize, Vec<u8>)> {
     let file = File::open(path)?;
@@ -22,19 +22,27 @@ fn read_mninst_file(path: &str) -> std::io::Result<(usize, Vec<u8>)> {
     if magic_number > 2049 {
         let x = reader.read_u32::<BigEndian>()?;
         let y = reader.read_u32::<BigEndian>()?;
+        println!("{}", x);
+        println!("{}", y);
     }
 
-    let mut buffer = Vec::new();
+    let mut buffer = Vec::with_capacity(num_items);
     reader.read_to_end(&mut buffer)?;
 
     Ok((num_items, buffer))
 }
 
-pub fn get_train_images() -> (Array2<u8>, Array1<u8>) {
+pub fn get_train_images() -> (TensorData, TensorData) {
     let (train_images_num, train_images_data) = read_mninst_file(TRAIN_IMAGES_FILE).unwrap();
-    let (label_images_num, label_images_data) = read_mninst_file(TRAIN_LABELS_FILE).unwrap();
-    let image_array =
-        Array::from_shape_vec((train_images_num, IMAGE_SIZE), train_images_data).unwrap();
-    let label_array = Array::from_shape_vec(label_images_num, label_images_data).unwrap();
-    (image_array, label_array)
+    let (train_labels_num, train_labels_data) = read_mninst_file(TRAIN_LABELS_FILE).unwrap();
+
+    //let image_array = Array::from_shape_vec((train_images_num, 28, 28), train_images_data).unwrap();
+    //let label_array = Array::from_shape_vec(train_labels_num, train_labels_data).unwrap();
+
+    let train_images_tensor_data =
+        TensorData::new(train_images_data, vec![train_images_num, 28, 28]);
+
+    let train_labels_tensor_data = TensorData::new(train_labels_data, vec![train_labels_num]);
+
+    (train_images_tensor_data, train_labels_tensor_data)
 }
